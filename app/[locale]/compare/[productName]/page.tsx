55109c1457f90642bce59002c4285543c5514b5b
Marian Wadia <marianwadia55@gmail.com>
2025-12-28 06:41:21 +0200

    added compareResults page

---
+++ app/[locale]/compare/[productName]/page.tsx
@@ -0,0 +1,12 @@
+import ComparisonResults from "@/app/shared/comapre";
+import React from "react";
+
+const ComparisonResultsPage = () => {
+	return (
+		<section className="pt-8 h-full w-full flex flex-col">
+			<ComparisonResults />
+		</section>
+	);
+};
+
+export default ComparisonResultsPage;
